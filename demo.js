b6a1674f3a190614a81ecacfd83b97aa540a8b21
Max Ogden <max@maxogden.com>
2013-04-04 20:59:17 -0700

    0.0.0

---
+++ demo.js
@@ -0,0 +1,45 @@
+var textures = "http://commondatastorage.googleapis.com/voxeltextures/"
+var instructions = document.querySelector('#blocker')
+var images = document.querySelector('#images')
+var exportButton = document.querySelector('#export')
+var resetButton = document.querySelector('#reset')
+
+var game = require('voxel-hello-world')({
+  texturePath: textures,
+  playerSkin: textures + 'player.png',
+  interactElement: instructions,
+  container: document.querySelector('#left'),
+  statsDisabled: true
+})
+
+game.interact.on('attain', function() { instructions.style.visibility = 'hidden' })
+game.interact.on('release', function() { instructions.style.visibility = 'visible' })
+
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode === 13) shutter()
+})
+
+exportButton.addEventListener('click', function(e) {
+  if (images.innerHTML === "") return
+  window.open(stopMotion.export())
+})
+
+resetButton.addEventListener('click', function(e) {
+  images.innerHTML = ""
+  stopMotion.createEncoder()
+})
+
+function shutter() {
+  var imageURI = stopMotion.shutter()
+  var img = new Image()
+  img.src = imageURI
+  images.appendChild(img)
+}
+
+var stopMotion = require('voxel-stop-motion')(game)
+window.stop = stopMotion
+
+stopMotion.cam.position.y = 7
+stopMotion.cam.position.z = 10
+stop.cam.rotation.x = -0.125
+
